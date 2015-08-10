9ebe19c4028c7308fdb79ee80b5f387ed71a6931
GathoMauricio <mauricio2769@gmail.com>
2015-08-10 17:28:54 -0500

    sd

---
+++ www/js/index.js
@@ -0,0 +1,34 @@
+// Wait for device API libraries to load
+    //
+    var num=1;
+    function onLoad() {
+        document.addEventListener("deviceready", onDeviceReady, false);
+        
+        Concurrent.Thread.create(function(){
+            while(1)
+            {
+                $("h1").append("En función "+num+"<br>");
+                Concurrent.Thread.sleep(1000);
+                num=num+1;
+            }
+        });
+    }
+
+    // device APIs are available
+    //
+    function onDeviceReady() {
+        document.addEventListener("pause", onPause, false);
+    }
+
+    // Handle the pause event
+    //
+    function onPause() {
+        Concurrent.Thread.create(function(){
+            while(1)
+            {
+                $("h1").append("En pause "+num+"<br>");
+                Concurrent.Thread.sleep(1000);
+                num=num+1;
+            }
+        });
+    }
